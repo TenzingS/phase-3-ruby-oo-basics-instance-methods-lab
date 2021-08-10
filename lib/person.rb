97b2bd11b0456ad99267ea152b56ec59911bdc65
TenzingS <thesherpatenzing@gmail.com>
2021-08-10 18:20:49 -0400

    Done

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+end
+
+t = Person.new
+t.talk
+t.walk
+
+w = Person.new
+w.walk
